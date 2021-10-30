2d2461792d9a921f297313f97954179f6b84dd4b
Itros97 <61584391+Itros97@users.noreply.github.com>
2021-10-30 14:33:45 +0200

    login user de marcos

---
+++ Client/gui/logginUser.java
@@ -0,0 +1,75 @@
+-package gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.CardLayout;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+
+public class logginUser extends JFrame {
+    private JTextField txtInsertUser;
+    private JPasswordField passwordField;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    logginUser frame = new logginUser();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public logginUser() {
+        setTitle("logginMainFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 500, 385);
+        getContentPane().setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("User e-mail");
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setFont(new Font("Calibri", Font.PLAIN, 30));
+        lblNewLabel.setBounds(70, 50, 150, 30);
+        getContentPane().add(lblNewLabel);
+
+        txtInsertUser = new JTextField();
+        txtInsertUser.setToolTipText("insert user");
+        txtInsertUser.setFont(new Font("Arial", Font.PLAIN, 15));
+        txtInsertUser.setBounds(40, 100, 400, 25);
+        getContentPane().add(txtInsertUser);
+        txtInsertUser.setColumns(10);
+
+        JLabel lblNewLabel_1 = new JLabel("User e-mail password");
+        lblNewLabel_1.setFont(new Font("Calibri", Font.PLAIN, 30));
+        lblNewLabel_1.setBounds(70, 170, 300, 30);
+        getContentPane().add(lblNewLabel_1);
+
+        JButton btnNewButton = new JButton("Loggin");
+        btnNewButton.setBounds(190, 290, 88, 25);
+        getContentPane().add(btnNewButton);
+
+        passwordField = new JPasswordField();
+        passwordField.setToolTipText("insert password");
+        passwordField.setFont(new Font("Arial", Font.PLAIN, 15));
+        passwordField.setBounds(40, 220, 400, 25);
+        getContentPane().add(passwordField);
+    }
+}
