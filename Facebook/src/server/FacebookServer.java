6452f25b6022a9f4f03c3b6359368399b21a3ba0
QuimiJ <55583482+QuimiJ@users.noreply.github.com>
2021-12-30 17:37:20 +0100

    FacebookServer definitivo.

--- Facebook/src/server/FacebookServer.java
+++ Facebook/src/server/FacebookServer.java
@@ -1,42 +1,32 @@
-/*package server;
+package server;
 
 import remote.FacebookService;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 
 public class FacebookServer {
+
+    private static int numClients = 0;
+
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.err.println(" # Usage: TCPSocketEchoServer [PORT]");
+            System.out.println(" # Usage: TranslationServer [PORT]");
             System.exit(1);
         }
 
         //args[1] = Server socket port
         int serverPort = Integer.parseInt(args[0]);
 
-        //Declaration of the ServerSocket (only a port number is needed)
-        try (ServerSocket tcpFacebookServerSocket = new ServerSocket(serverPort);) {
-            System.out.println(" - FacebookServer: Waiting for connections '" + tcpFacebookServerSocket.getInetAddress().getHostAddress() + ":" + tcpFacebookServerSocket.getLocalPort() + "' ...");
+        try (ServerSocket tcpServerSocket = new ServerSocket(serverPort);) {
+            System.out.println(" - FacebookServer: Waiting for connections '" + tcpServerSocket.getInetAddress().getHostAddress() + ":" + tcpServerSocket.getLocalPort() + "' ...");
 
-            //The main thread is always waiting for connections
             while (true) {
-                //When a connection from a client is received, a new EchoService is created. The "accept()" method returns the socket to
-                //communicate with the client.
-                new FacebookService(tcpFacebookServerSocket.accept());
-                System.out.println(" - FacebookServer: New client connection accepted. Client Number: " + numClients++);
+                new FacebookService(tcpServerSocket.accept());
+                System.out.println(" - FacebookServer: New client connection accepted. Client number: " + ++numClients);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("# FacebookServer: IO error:" + e.getMessage());
-        }
-
-        try {
-            IFacebook remoteObject = FacebookService.getInstance();
-            Naming.rebind(name, remoteObject);
-            System.out.println(" * Google Server '" + name + "' started!!");
-        } catch (Exception ex) {
-            System.out.println(" # Google Server: " + ex.getMessage());
-            ex.printStackTrace();
         }
     }
 }
-*/
