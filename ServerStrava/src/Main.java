8a6c4ccd6a2ab8b50d1ff82314fc98432eef06f7
QuimiJ <55583482+QuimiJ@users.noreply.github.com>
2022-01-19 19:49:20 +0100

    Entidades en la BD.

--- ServerStrava/src/Main.java
+++ ServerStrava/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
         try {
             IRemoteFacade remoteFacade = new RemoteFacade();
             Naming.rebind(name, remoteFacade);
-            System.setProperty("java.rmi.server.hostname","127.0.0.1");
+            //System.setProperty("java.rmi.server.hostname","127.0.0.1");
             System.out.println(" * STRAVA server '" + name + "' started!!");
         } catch (Exception ex) {
             System.err.println(" # STRAVA Server Exception: " + ex.getMessage());
@@ -97,59 +97,59 @@ public class Main {
             ProfileDAO.getInstance().save(p5);
 
             PasswordProfile pp1 = new PasswordProfile();
-            pp1.setEmail("elenagonzalez@gmail.com");
-            pp1.setNickname("El");
-            pp1.setBirthdate();
-            pp1.setWeightKg();
-            pp1.setHeightCm();
-            pp1.setMaximumHeartRate();
-            pp1.setReposeHeartRate();
-            pp1.setRegisterType();
-            pp1.setPassword("");
+            pp1.setEmail("denna@gmail.com");
+            pp1.setNickname("Chand");
+            pp1.setBirthdate(calendar);
+            pp1.setWeightKg(70.4);
+            pp1.setHeightCm(174);
+            pp1.setMaximumHeartRate(175);
+            pp1.setReposeHeartRate(80);
+            pp1.setRegisterType("");
+            pp1.setPassword("password");
 
             PasswordProfile pp2 = new PasswordProfile();
-            pp2.setEmail("elenagonzalez@gmail.com");
-            pp2.setNickname("El");
-            pp2.setBirthdate();
-            pp2.setWeightKg();
-            pp2.setHeightCm();
-            pp2.setMaximumHeartRate();
-            pp2.setReposeHeartRate();
-            pp2.setRegisterType();
-            pp2.setPassword("");
+            pp2.setEmail("ambrose@gmail.com");
+            pp2.setNickname("Jackass");
+            pp2.setBirthdate(calendar);
+            pp2.setWeightKg(140.4);
+            pp2.setHeightCm(170);
+            pp2.setMaximumHeartRate(175);
+            pp2.setReposeHeartRate(80);
+            pp2.setRegisterType("google");
+            pp2.setPassword("qwerty");
 
             PasswordProfile pp3 = new PasswordProfile();
-            pp3.setEmail("elenagonzalez@gmail.com");
-            pp3.setNickname("El");
-            pp3.setBirthdate();
-            pp3.setWeightKg();
-            pp3.setHeightCm();
-            pp3.setMaximumHeartRate();
-            pp3.setReposeHeartRate();
-            pp3.setRegisterType();
-            pp3.setPassword("");
+            pp3.setEmail("bredon@gmail.com");
+            pp3.setNickname("Ceniza");
+            pp3.setBirthdate(calendar);
+            pp3.setWeightKg(68.3);
+            pp3.setHeightCm(183);
+            pp3.setMaximumHeartRate(175);
+            pp3.setReposeHeartRate(80);
+            pp3.setRegisterType("facebook");
+            pp3.setPassword("asdfg");
 
             PasswordProfile pp4 = new PasswordProfile();
-            pp4.setEmail("elenagonzalez@gmail.com");
-            pp4.setNickname("El");
-            pp4.setBirthdate();
-            pp4.setWeightKg();
-            pp4.setHeightCm();
-            pp4.setMaximumHeartRate();
-            pp4.setReposeHeartRate();
-            pp4.setRegisterType();
-            pp4.setPassword("");
+            pp4.setEmail("ilodin@gmail.com");
+            pp4.setNickname("TaborlinTheGreat");
+            pp4.setBirthdate(calendar);
+            pp4.setWeightKg(64.1);
+            pp4.setHeightCm(190);
+            pp4.setMaximumHeartRate(175);
+            pp4.setReposeHeartRate(80);
+            pp4.setRegisterType("");
+            pp4.setPassword("yuiop");
 
             PasswordProfile pp5 = new PasswordProfile();
-            pp5.setEmail("elenagonzalez@gmail.com");
-            pp5.setNickname("El");
-            pp5.setBirthdate();
-            pp5.setWeightKg();
-            pp5.setHeightCm();
-            pp5.setMaximumHeartRate();
-            pp5.setReposeHeartRate();
-            pp5.setRegisterType();
-            pp5.setPassword("");
+            pp5.setEmail("tunnelauri@gmail.com");
+            pp5.setNickname("Bob");
+            pp5.setBirthdate(calendar);
+            pp5.setWeightKg(61.7);
+            pp5.setHeightCm(162);
+            pp5.setMaximumHeartRate(175);
+            pp5.setReposeHeartRate(80);
+            pp5.setRegisterType("google");
+            pp5.setPassword("electricboogaloo");
 
             //Save Users in the DB
             PasswordProfileDAO.getInstance().save(pp1);
@@ -159,54 +159,44 @@ public class Main {
             PasswordProfileDAO.getInstance().save(pp5);
 
             TrainingSession ts1 = new TrainingSession();
-            ts1.setTitle();
-            ts1.setSport();
-            ts1.setDistance();
-            ts1.setDistance();
-            ts1.setDistance();
-            ts1.setDateOfStart();
-            ts1.setDuration();
-            ts1.setAccepted();
+            ts1.setTitle("Running");
+            ts1.setSport("correr");
+            ts1.setDistance(12);
+            ts1.setDateOfStart(calendar);
+            ts1.setDuration(60);
+            ts1.setAccepted(false);
 
             TrainingSession ts2 = new TrainingSession();
-            ts2.setTitle();
-            ts2.setSport();
-            ts2.setDistance();
-            ts2.setDistance();
-            ts2.setDistance();
-            ts2.setDateOfStart();
-            ts2.setDuration();
-            ts2.setAccepted();
+            ts2.setTitle("Biciclismo");
+            ts2.setSport("ciclismo");
+            ts2.setDistance(24);
+            ts2.setDateOfStart(calendar);
+            ts2.setDuration(60);
+            ts2.setAccepted(false);
 
             TrainingSession ts3 = new TrainingSession();
-            ts3.setTitle();
-            ts3.setSport();
-            ts3.setDistance();
-            ts3.setDistance();
-            ts3.setDistance();
-            ts3.setDateOfStart();
-            ts3.setDuration();
-            ts3.setAccepted();
+            ts3.setTitle("Natacion");
+            ts3.setSport("natacion");
+            ts3.setDistance(10);
+            ts3.setDateOfStart(calendar);
+            ts3.setDuration(200);
+            ts3.setAccepted(false);
 
             TrainingSession ts4 = new TrainingSession();
-            ts4.setTitle();
-            ts4.setSport();
-            ts4.setDistance();
-            ts4.setDistance();
-            ts4.setDistance();
-            ts4.setDateOfStart();
-            ts4.setDuration();
-            ts4.setAccepted();
+            ts4.setTitle("Correr");
+            ts4.setSport("correr");
+            ts4.setDistance(20);
+            ts4.setDateOfStart(calendar);
+            ts4.setDuration(120);
+            ts4.setAccepted(false);
 
             TrainingSession ts5 = new TrainingSession();
-            ts5.setTitle();
-            ts5.setSport();
-            ts5.setDistance();
-            ts5.setDistance();
-            ts5.setDistance();
-            ts5.setDateOfStart();
-            ts5.setDuration();
-            ts5.setAccepted();
+            ts5.setTitle("Cycling");
+            ts5.setSport("ciclismo");
+            ts5.setDistance(40);
+            ts5.setDateOfStart(calendar);
+            ts5.setDuration(120);
+            ts5.setAccepted(false);
 
             //Save Users in the DB
             TrainingSessionDAO.getInstance().save(ts1);
@@ -216,39 +206,39 @@ public class Main {
             TrainingSessionDAO.getInstance().save(ts5);
 
             Challenge ch1 = new Challenge();
-            ch1.setName();
-            ch1.setDateOfStart();
-            ch1.setDateOfEnd();
-            ch1.setSport();
-            ch1.setAccepted();
+            ch1.setName("Correr como Usain Bolt");
+            ch1.setDateOfStart(calendar);
+            ch1.setDateOfEnd(calendar);
+            ch1.setSport("correr");
+            ch1.setAccepted(false);
 
             Challenge ch2 = new Challenge();
-            ch2.setName();
-            ch2.setDateOfStart();
-            ch2.setDateOfEnd();
-            ch2.setSport();
-            ch2.setAccepted();
+            ch2.setName("Nadar un kilometro");
+            ch2.setDateOfStart(calendar);
+            ch2.setDateOfEnd(calendar);
+            ch2.setSport("natacion");
+            ch2.setAccepted(false);
 
             Challenge ch3 = new Challenge();
-            ch3.setName();
-            ch3.setDateOfStart();
-            ch3.setDateOfEnd();
-            ch3.setSport();
-            ch3.setAccepted();
+            ch3.setName("Subir una colina");
+            ch3.setDateOfStart(calendar);
+            ch3.setDateOfEnd(calendar);
+            ch3.setSport("ciclismo");
+            ch3.setAccepted(false);
 
             Challenge ch4 = new Challenge();
-            ch4.setName();
-            ch4.setDateOfStart();
-            ch4.setDateOfEnd();
-            ch4.setSport();
-            ch4.setAccepted();
+            ch4.setName("Subir una montanya");
+            ch4.setDateOfStart(calendar);
+            ch4.setDateOfEnd(calendar);
+            ch4.setSport("ciclismo");
+            ch4.setAccepted(false);
 
             Challenge ch5 = new Challenge();
-            ch5.setName();
-            ch5.setDateOfStart();
-            ch5.setDateOfEnd();
-            ch5.setSport();
-            ch5.setAccepted();
+            ch5.setName("Nadar un rato");
+            ch5.setDateOfStart(calendar);
+            ch5.setDateOfEnd(calendar);
+            ch5.setSport("natacion");
+            ch5.setAccepted(false);
 
             //Save Users in the DB
             ChallengeDAO.getInstance().save(ch1);
@@ -258,44 +248,44 @@ public class Main {
             ChallengeDAO.getInstance().save(ch5);
 
             ChallengeWithDistance chd1 = new ChallengeWithDistance();
-            chd1.setName();
-            chd1.setDateOfStart();
-            chd1.setDateOfEnd();
-            chd1.setSport();
-            chd1.setObjectiveDistance();
-            chd1.setAccepted();
+            chd1.setName("Correr como un rayo");
+            chd1.setDateOfStart(calendar);
+            chd1.setDateOfEnd(calendar);
+            chd1.setSport("correr");
+            chd1.setObjectiveDistance(30.5);
+            chd1.setAccepted(false);
 
             ChallengeWithDistance chd2 = new ChallengeWithDistance();
-            chd2.setName();
-            chd2.setDateOfStart();
-            chd2.setDateOfEnd();
-            chd2.setSport();
-            chd2.setObjectiveDistance();
-            chd2.setAccepted();
+            chd2.setName("Nadar dos kilometros");
+            chd2.setDateOfStart(calendar);
+            chd2.setDateOfEnd(calendar);
+            chd2.setSport("natacion");
+            chd2.setObjectiveDistance(2);
+            chd2.setAccepted(false);
 
             ChallengeWithDistance chd3 = new ChallengeWithDistance();
-            chd3.setName();
-            chd3.setDateOfStart();
-            chd3.setDateOfEnd();
-            chd3.setSport();
-            chd3.setObjectiveDistance();
-            chd3.setAccepted();
+            chd3.setName("Subir una colina en tu bici");
+            chd3.setDateOfStart(calendar);
+            chd3.setDateOfEnd(calendar);
+            chd3.setSport("ciclismo");
+            chd3.setObjectiveDistance(0.5);
+            chd3.setAccepted(false);
 
             ChallengeWithDistance chd4 = new ChallengeWithDistance();
-            chd4.setName();
-            chd4.setDateOfStart();
-            chd4.setDateOfEnd();
-            chd4.setSport();
-            chd4.setObjectiveDistance();
-            chd4.setAccepted();
+            chd4.setName("Subir el Gorbea");
+            chd4.setDateOfStart(calendar);
+            chd4.setDateOfEnd(calendar);
+            chd4.setSport("ciclismo");
+            chd4.setObjectiveDistance(8.2);
+            chd4.setAccepted(false);
 
             ChallengeWithDistance chd5 = new ChallengeWithDistance();
-            chd5.setName();
-            chd5.setDateOfStart();
-            chd5.setDateOfEnd();
-            chd5.setSport();
-            chd5.setObjectiveDistance();
-            chd5.setAccepted();
+            chd5.setName("Nadar un ratito");
+            chd5.setDateOfStart(calendar);
+            chd5.setDateOfEnd(calendar);
+            chd5.setSport("natacion");
+            chd5.setObjectiveDistance(3.2);
+            chd5.setAccepted(false);
 
             //Save Users in the DB
             ChallengeWithDistanceDAO.getInstance().save(chd1);
@@ -305,44 +295,44 @@ public class Main {
             ChallengeWithDistanceDAO.getInstance().save(chd5);
 
             ChallengeWithTime cht1 = new ChallengeWithTime();
-            cht1.setName();
-            cht1.setDateOfStart();
-            cht1.setDateOfEnd();
-            cht1.setSport();
-            cht1.setObjectiveTime();
-            cht1.setAccepted();
+            cht1.setName("Correr para entrenar");
+            cht1.setDateOfStart(calendar);
+            cht1.setDateOfEnd(calendar);
+            cht1.setSport("correr");
+            cht1.setObjectiveTime(300);
+            cht1.setAccepted(false);
 
             ChallengeWithTime cht2 = new ChallengeWithTime();
-            cht2.setName();
-            cht2.setDateOfStart();
-            cht2.setDateOfEnd();
-            cht2.setSport();
-            cht2.setObjectiveTime();
-            cht2.setAccepted();
+            cht2.setName("Nadar tres kilometros");
+            cht2.setDateOfStart(calendar);
+            cht2.setDateOfEnd(calendar);
+            cht2.setSport("natacion");
+            cht2.setObjectiveTime(60);
+            cht2.setAccepted(false);
 
             ChallengeWithTime cht3 = new ChallengeWithTime();
-            cht3.setName();
-            cht3.setDateOfStart();
-            cht3.setDateOfEnd();
-            cht3.setSport();
-            cht3.setObjectiveTime();
-            cht3.setAccepted();
+            cht3.setName("Paseo en bici");
+            cht3.setDateOfStart(calendar);
+            cht3.setDateOfEnd(calendar);
+            cht3.setSport("ciclismo");
+            cht3.setObjectiveTime(30);
+            cht3.setAccepted(false);
 
             ChallengeWithTime cht4 = new ChallengeWithTime();
-            cht4.setName();
-            cht4.setDateOfStart();
-            cht4.setDateOfEnd();
-            cht4.setSport();
-            cht4.setObjectiveTime();
-            cht4.setAccepted();
+            cht4.setName("Ruta en bici");
+            cht4.setDateOfStart(calendar);
+            cht4.setDateOfEnd(calendar);
+            cht4.setSport("ciclismo");
+            cht4.setObjectiveTime(120);
+            cht4.setAccepted(false);
 
             ChallengeWithTime cht5 = new ChallengeWithTime();
-            cht5.setName();
-            cht5.setDateOfStart();
-            cht5.setDateOfEnd();
-            cht5.setSport();
-            cht5.setObjectiveTime();
-            cht5.setAccepted();
+            cht5.setName("Nadar un largo rato");
+            cht5.setDateOfStart(calendar);
+            cht5.setDateOfEnd(calendar);
+            cht5.setSport("natacion");
+            cht5.setObjectiveTime(50);
+            cht5.setAccepted(false);
 
             //Save Users in the DB
             ChallengeWithTimeDAO.getInstance().save(cht1);
